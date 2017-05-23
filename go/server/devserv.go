b005cfcca5c50ca87cf7c5a5680c5788189079fe
geobe <code@georgbeier.de>
2017-05-23 08:42:09 +0200

    test version to observe redirect behaviour

--- go/server/devserv.go
+++ go/server/devserv.go
@@ -5,7 +5,14 @@ import (
 	"github.com/geobe/gostip/go/model"
 	"net/http"
 	"log"
+	//"net"
+	"net/url"
+	"strings"
 )
+
+const httpport = ":8070"
+const tlsport = ":8090"
+const schema  = "http"
 
 func main() {
 	// prepare database
@@ -19,11 +26,37 @@ func main() {
 
 	// konfiguriere server
 	server := &http.Server{
-		//Addr: "127.0.0.1:8090",
-		Addr:    "0.0.0.0:8090",
+		Addr:    "0.0.0.0" + tlsport,
 		Handler: mux,
 	}
-	// und starte ihn
+
+	// konfiguriere redirect server
+	redirectserver := &http.Server{
+		Addr:    "0.0.0.0" + httpport,
+		Handler: http.HandlerFunc(RedirectHTTP),
+	}
+	// starte den redirect server
+	go redirectserver.ListenAndServe()
+
+	// und starte den primären server
+	log.Printf("server starting\n")
 	server.ListenAndServe()
-	log.Printf("server started\n")
+}
+
+
+// RedirectHTTP is an HTTP handler (suitable for use with http.HandleFunc)
+// that responds to all requests by redirecting to the same URL served over HTTPS.
+// It should only be invoked for requests received over HTTP.
+func RedirectHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.TLS != nil || r.Host == "" {
+		http.Error(w, "not found", 404)
+	}
+
+	var u *url.URL
+	u = r.URL
+	host := r.Host
+	u.Host = strings.Split(host, ":")[0] + tlsport
+	u.Scheme = schema
+	log.Printf("redirect to u.host  %s -> %s\n", r.Host, u.String())
+	http.Redirect(w, r, u.String(), 302)
 }
