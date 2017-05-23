79b0838a1c39abf7b38aa0e3dca32d56c1afea37
geobe <code@georgbeier.de>
2017-05-23 11:06:18 +0200

    destinguish between local and internet access

--- go/server/devserv.go
+++ go/server/devserv.go
@@ -8,11 +8,12 @@ import (
 	//"net"
 	"net/url"
 	"strings"
+	"regexp"
 )
 
 const httpport = ":8070"
 const tlsport = ":8090"
-const schema  = "http"
+const schema = "http"
 
 func main() {
 	// prepare database
@@ -30,10 +31,16 @@ func main() {
 		Handler: mux,
 	}
 
+	// switching redirect handler
+	handlerSwitch := &HandlerSwitch{
+		Mux:    mux,
+		Redirect: http.HandlerFunc(RedirectHTTP),
+	}
+
 	// konfiguriere redirect server
 	redirectserver := &http.Server{
 		Addr:    "0.0.0.0" + httpport,
-		Handler: http.HandlerFunc(RedirectHTTP),
+		Handler: handlerSwitch, //http.HandlerFunc(RedirectHTTP),
 	}
 	// starte den redirect server
 	go redirectserver.ListenAndServe()
@@ -59,4 +66,22 @@ func RedirectHTTP(w http.ResponseWriter, r *http.Request) {
 	u.Scheme = schema
 	log.Printf("redirect to u.host  %s -> %s\n", r.Host, u.String())
 	http.Redirect(w, r, u.String(), 302)
+}
+
+type HandlerSwitch struct {
+	Mux      http.Handler
+	Redirect http.Handler
+}
+
+// nicht richtig für 172.16.0.0/12
+var matcher = regexp.MustCompile("(192\\.168.*)|(localhost)|(10\\..*)|(172\\..*)")
+
+func (h *HandlerSwitch) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	host := r.Host
+	local := matcher.MatchString(host)
+	if local {
+		h.Mux.ServeHTTP(w, r)
+	} else {
+		h.Redirect.ServeHTTP(w, r)
+	}
 }
